53b12a53208c335c28d56459ec0a352611fcd1f4
Sebastian <sebastiandiaz1222@gmail.com>
2024-04-13 17:07:39 -0700

    Add files via upload

---
+++ graph-visualizer/src/main/java/com/graphapp/graphvisualizer/GraphVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.graphapp.graphvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphVisualizerApplication.class, args);
+	}
+
+}
---
+++ graph-visualizer/src/test/java/com/graphapp/graphvisualizer/GraphVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphapp.graphvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
