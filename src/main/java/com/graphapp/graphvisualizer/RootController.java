c6c3d01dba634aee3a1fd1ccf21404ad8b8972c5
Colin <cjcanaday@gmail.com>
2024-04-14 15:55:03 -0700

    Added basic query functionality

--- src/main/java/com/graphapp/graphvisualizer/RootController.java
+++ src/main/java/com/graphapp/graphvisualizer/RootController.java
@@ -3,13 +3,15 @@ package com.graphapp.graphvisualizer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/")
 public class RootController {
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, @RequestParam(value="value", required=false, defaultValue="World") String value) {
+        model.addAttribute("value", value);
         return "index";
     }
 
