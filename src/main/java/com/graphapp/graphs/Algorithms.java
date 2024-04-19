52692b0401494304d26ec151c385d3e12575271b
Colin <cjcanaday@gmail.com>
2024-04-18 23:36:38 -0700

    BFS initial commit

--- src/main/java/com/graphapp/graphs/Algorithms.java
+++ src/main/java/com/graphapp/graphs/Algorithms.java
@@ -4,18 +4,51 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Algorithms {
 
-    public ArrayList<String> bfs(Graph g, Node start, Node end) {
+    public ArrayList<String> bfs(Graph g, Node start) {
+        HashMap<Node, Integer> vertices = g.getVertices();
+
+        if(vertices.get(start) == null) {
+            return null; // need error handling
+        }
+        ArrayList<String> result = new ArrayList<>();
+        boolean[] visitedVertices = new boolean[g.getNumVertices()];
+        int[][] adj = g.getAdjMatrix();
+        Arrays.fill(visitedVertices,false); // set all visited to False
+
+        ArrayList<Node> queue = new ArrayList<>();
+
+        queue.add(start);
+        visitedVertices[vertices.get(start)] = true;
+
+        int currIdx;
+        while (!queue.isEmpty()) {
+            Node current = queue.get(0);
+            currIdx = vertices.get(current);
+
+            result.add(current.getLabel()); // add found node to result
+
+            queue.remove(0);
+
+            for(int i = 0; i < adj.length; i++) {
+                if(adj[currIdx][i] == 1 && !visitedVertices[currIdx]) {
+                    queue.add(g.getVertex(i));
+                    visitedVertices[i] = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public ArrayList<String> dfs(Graph g, Node start) {
         return null;
     }
 
-    public ArrayList<String> dfs(Graph g, Node start, Node end) {
-        return null;
-    }
-
-    public ArrayList<String> djikstra(Graph g, Node start, Node end) {
+    public ArrayList<String> djikstra(Graph g, Node start) {
         return null;
     }
 }
